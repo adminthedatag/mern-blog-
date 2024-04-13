42e1f186a0235b59cb37d840108d612c7e9f1e5c
eltosa <sebastian_tosa@hotmail.com>
2024-04-13 10:36:11 -0500

    fix middleware funtion

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email ==='' || password ==='') {
-        return res.status(400).json({ message: 'All fields are required' });
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashPassword = bcryptjs.hashSync(password, 10);
