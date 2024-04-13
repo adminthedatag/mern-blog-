c0b29be52ad469a345aa050e8e81d1bb1cb5692d
eltosa <sebastian_tosa@hotmail.com>
2024-04-13 09:42:29 -0500

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,7 +21,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-      
+        <Footer></Footer>
 
 
     </BrowserRouter>
