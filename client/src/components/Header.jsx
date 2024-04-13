eec53a8323f93a07cf44463ac526289c5ab73060
eltosa <sebastian_tosa@hotmail.com>
2024-04-13 11:43:34 -0500

    add button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,6 +32,9 @@ export default function Header() {
           <Link to ='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
           </Link>
+          <Link to ='/sign-up'>
+            <Button gradientDuoTone='purpleToBlue' outline>Sign Up</Button>
+          </Link>
           <Navbar.Toggle />
           </div>
           <Navbar.Collapse>
